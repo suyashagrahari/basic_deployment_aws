0b33731b1dc585538a3b1abce667fa76d86e844b
suyashagrahari <suyashagrahari73@gmail.com>
2024-11-18 13:34:54 +0530

    adding first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+// Create an instance of Express
+const app = express();
+
+// Define the '/' endpoint
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Set the server to listen on a specific port
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
